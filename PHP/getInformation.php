f600b8abe135bef7776b933538e3eae05016dbf9
eun-seong <eunseongofficial@gmail.com>
2020-07-30 22:26:10 +0900

    add getInformation

--- PHP/getInformation.php
+++ PHP/getInformation.php
@@ -1,20 +1,29 @@
 <?php
 $apiKey = "5d48c9b4320dc403008c5d2f2f1d2e63";
 $cityId = "1835848";
-$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&lang=en&units=metric&APPID=" . $apiKey;
+$ApiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" . $cityId . "&appid=" . $apiKey;
 
-$ch = curl_init();
 
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_VERBOSE, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$response = curl_exec($ch);
+$request = file_get_contents($ApiUrl); //example ID
 
-curl_close($ch);
-$data = json_decode($response);
-$currentTime = time();
-echo $data;
+$jsonPHP  = json_decode($request,true);
+
+$weather_desc = $jsonPHP["weather"][0]["description"];
+$temp = $jsonPHP["main"]["temp"];
+
+try {
+    $stmt = $con->prepare('UPDATE ino_data SET weather_desc=(:weather_desc), temp=(:temp)');
+    $stmt->bindParam(':weather_desc', $weather_desc);
+    $stmt->bindParam(':temp', $temp);
+
+    if ($stmt->execute()) {
+      $successMSG = "날씨 정보 업데이트 성공";
+    } else {
+      $errMSG = "날씨 정보 업데이트 실패";
+    }
+  } catch (PDOException $e) {
+    die("Database error: " . $e->getMessage());
+  }
+
+echo "\nfinish";
 ?>
